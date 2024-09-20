476e958fc29134ae947c96034fafde4ce4705dfc
fay-ui <faith.njau@student.moringaschool.com>
2024-09-20 14:21:36 +0300

    initial commit

---
+++ practice.js
@@ -0,0 +1,26 @@
+//arrays
+let names=['faith','njau','kelvin']
+console.log(names);
+console.log(names.length);
+names.push='miriti';
+console.log(names)
+names[2]='ann';
+console.log(names);
+var x=10;
+y=11;
+let sum =x + y ;
+console.log(sum);
+var firstName='faith';
+lastName='njau';
+let combine=(firstName )+ "" +(lastName);
+console.log(combine);
+
+
+
+
+
+
+
+
+
+
